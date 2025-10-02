b767542cf70c403aabee0331213d4c07e9def80f
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-02 23:57:59 +0900

    update: add shadcn-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
+  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
+  css: ['~/assets/css/tailwind.css'],
+  alias: {
+    '@': './app'
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
 })
