13336c44a70d59c9e8d91a4db296f5f9693978be
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-03 00:10:40 +0900

    fix: shadcn-ui foloder

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Index page</h1>
+  <Button>Click me</Button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,23 +6,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', 'shadcn-nuxt'],
   css: ['~/assets/css/tailwind.css'],
-  alias: {
-    '@': './app'
-  },
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
   shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
     prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: './components/ui'
+    componentDir: './app/components/ui'
   },
 })
