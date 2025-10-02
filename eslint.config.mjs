0d08740dd1bd67850a6cffca81cb001eab36ba72
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-03 00:20:42 +0900

    update: add lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
+export default withNuxt({
   // Your custom configs here
-)
+  ignores: [
+    '.vscode/**',
+    'app/components/ui'
+  ],
+})
