146024042e0ca1791275d1a2577728df6efbba69
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-04 12:16:31 +0900

    update: add eslintconfigPrettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-export default withNuxt({
-  // Your custom configs here
-  ignores: [".vscode/**", "app/components/ui"],
-});
+export default withNuxt([
+  eslintConfigPrettier,
+  {
+    // Your custom configs here
+    ignores: [".vscode/**", "app/components/ui"],
+  },
+]);
