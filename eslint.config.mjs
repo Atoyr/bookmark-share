5c7271815d1482b714a41bcff47416dc2a2715f4
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-06 00:35:41 +0900

    update: lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,14 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default withNuxt([
+  {
+    rules: {
+      'vue/no-multiple-template-root': 'off',
+    },
+  },
   eslintConfigPrettier,
   {
     // Your custom configs here
-    ignores: ['.vscode/**', 'app/components/ui'],
+    ignores: ['.vscode/**', 'app/components/ui', 'eslint.config.mjs', 'prettier.config.ts'],
   },
 ]);
