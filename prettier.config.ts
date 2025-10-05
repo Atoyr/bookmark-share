f7eed32708df45da0cd66f3887e5876b3b539b94
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-05 23:06:50 +0900

    update: add prettier config file

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from 'prettier'
+
+const config: Config = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  vueIndentScriptAndStyle: true,
+}
+
+export default config
