c1ad02bc377b1fd2cba14c328f20e9e5b069cbb2
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-05 23:14:46 +0900

    update: apply prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "shadcn-nuxt"],
-  css: ["~/assets/css/tailwind.css"],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
+  css: ['~/assets/css/tailwind.css'],
   vite: {
     plugins: [tailwindcss()],
   },
   shadcn: {
-    prefix: "",
-    componentDir: "./app/components/ui",
+    prefix: '',
+    componentDir: './app/components/ui',
   },
 });
--- prettier.config.ts
+++ prettier.config.ts
@@ -1,13 +1,13 @@
-import { type Config } from 'prettier'
+import { type Config } from 'prettier';
 
 const config: Config = {
-  semi: false,
+  semi: true,
   singleQuote: true,
   printWidth: 120,
   tabWidth: 2,
   trailingComma: 'es5',
   bracketSpacing: true,
   vueIndentScriptAndStyle: true,
-}
+};
 
-export default config
+export default config;
