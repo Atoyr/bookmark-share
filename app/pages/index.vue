da250da2363eb060bc3b9cd0f9069d4643c705bb
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-07 22:28:38 +0900

    update: add sample pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,4 @@
 <template>
-  <h1>Index page</h1>
+  <h1 class="mx-8">Index page</h1>
   <Button>Click me</Button>
 </template>
---
+++ app/pages/spaces/[id].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+  import BookmarkGroupButton from './BookmarkGroupButton.vue';
+
+  const route = useRoute();
+
+  // When accessing /posts/1, route.params.id will be 1
+  console.log(route.params.id);
+</script>
+
+<template>
+  <div class="size-full bg-blue-500">
+    <div>id is {{ $route.params.id }}</div>
+    <BookmarkGroupButton />
+  </div>
+</template>
