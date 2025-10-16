4e5f281232d1984975b5ae6218cb4f2a9c7ef873
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-16 22:00:05 +0900

    GR

--- app/pages/index.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <h1 class="mx-8">Index page</h1>
-  <ShadButton>Click me</ShadButton>
-</template>
--- app/pages/spaces/BookmarkGroupButton.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="mx-8 flex flex-col">
-    <div class="mx-8 flex w-full">foo</div>
-    <div class="flex">boo</div>
-  </div>
-</template>
