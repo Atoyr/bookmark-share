c11a70e6655b9d0df3d30bececaa9ff9db84150d
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-16 23:25:55 +0900

    update: add sidebar

---
+++ app/components/AppSidebar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <ShadSidebar>
+    <ShadSidebarHeader />
+    <ShadSidebarContent>
+      <ShadSidebarGroup />
+      <ShadSidebarGroup />
+    </ShadSidebarContent>
+    <ShadSidebarFooter />
+  </ShadSidebar>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  import AppSidebar from '@/components/AppSidebar.vue';
+</script>
+
+<template>
+  <ShadSidebarProvider>
+    <AppSidebar />
+    <ShadSidebarInset>
+      <header class="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+        <ShadSidebarTrigger class="-ml-1" />
+        <ShadSeparator orientation="vertical" class="mr-2 h-4" />
+        <ShadBreadcrumb>
+          <ShadBreadcrumbList>
+            <ShadBreadcrumbItem class="hidden md:block">
+              <ShadBreadcrumbLink href="#">
+                Building Your Application
+              </ShadBreadcrumbLink>
+            </ShadBreadcrumbItem>
+            <ShadBreadcrumbSeparator class="hidden md:block" />
+            <ShadBreadcrumbItem>
+              <ShadBreadcrumbPage>Data Fetching</ShadBreadcrumbPage>
+            </ShadBreadcrumbItem>
+          </ShadBreadcrumbList>
+        </ShadBreadcrumb>
+      </header>
+    </ShadSidebarInset>
+    <main>
+      <slot />
+    </main>
+  </ShadSidebarProvider>
+</template>
