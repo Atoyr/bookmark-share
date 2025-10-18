97e6112e8d900e568306172f756066d223b11f87
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-19 00:56:29 +0900

    update: add bookmark item

--- app/pages/bookmarks/index.vue
+++ app/pages/bookmarks/index.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
   import { useBookmarks } from '@/composables/useBookmarks';
+  import { Item } from '@/components/bookmarks';
+
   const { bookmarks } = useBookmarks();
   console.log('book', bookmarks);
 </script>
 
 <template>
   <div v-for="bookmark in bookmarks">
-    <p>{{ bookmark.title }} | {{ bookmark.url }}</p>
+    <Item
+      :title="bookmark.title"
+      :url="bookmark.url"
+      :key="bookmark.id"
+    />
   </div>
 </template>
