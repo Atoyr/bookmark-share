345ba7b6b1011f87a0afdec2027afc3393cdd073
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-23 00:04:02 +0900

    update: add pading

--- app/pages/bookmarks/index.vue
+++ app/pages/bookmarks/index.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
   import { useBookmarks } from '@/composables/useBookmarks';
   import { columns } from '@/components/bookmarks';
-  import DataTable from '@/components/DataTable.vue'
+  import DataTable from '@/components/DataTable.vue';
 
   const { bookmarks } = useBookmarks();
   console.log('book', bookmarks);
 </script>
 
 <template>
-  <div class="container py-10 mx-auto">
-    <DataTable :columns="columns" :data="bookmarks" />
+  <div class="px-2">
+    <div class="container mx-auto py-10">
+      <DataTable
+        :columns="columns"
+        :data="bookmarks"
+      />
+    </div>
   </div>
 </template>
