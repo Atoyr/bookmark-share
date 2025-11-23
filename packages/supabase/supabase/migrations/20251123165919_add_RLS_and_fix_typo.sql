1c98855edb6b0a22458e547d24b1e8023476d632
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-11-24 02:00:26 +0900

    update: add mig

---
+++ packages/supabase/supabase/migrations/20251123165919_add_RLS_and_fix_typo.sql
@@ -0,0 +1,37 @@
+alter table "public"."users" drop column "avater";
+
+alter table "public"."users" add column "avatar" text;
+
+create policy "Enable insert for authenticated users only"
+on "public"."space_users"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable read access for all users"
+on "public"."space_users"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."spaces"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable read access for all users"
+on "public"."spaces"
+as permissive
+for select
+to public
+using (true);
+
+
+
